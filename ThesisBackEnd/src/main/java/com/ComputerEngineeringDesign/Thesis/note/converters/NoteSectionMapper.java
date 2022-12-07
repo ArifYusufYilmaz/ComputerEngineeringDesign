aaa183d502b02e9311ef72326a7a41692887451a
arif-yusuf-yilmaz3 <y.37ylmaz@gmail.com>
2022-12-07 21:14:15 +0300

    added noteSectionResponseDto List method

--- ThesisBackEnd/src/main/java/com/ComputerEngineeringDesign/Thesis/note/converters/NoteSectionMapper.java
+++ ThesisBackEnd/src/main/java/com/ComputerEngineeringDesign/Thesis/note/converters/NoteSectionMapper.java
@@ -1,13 +1,17 @@
 package com.ComputerEngineeringDesign.Thesis.note.converters;
 
+import com.ComputerEngineeringDesign.Thesis.note.dtos.noteSectionDtos.NoteSectionResponseDto;
+import com.ComputerEngineeringDesign.Thesis.note.entities.NoteSection;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface NoteSectionMapper {
     NoteSectionMapper INSTANCE = Mappers.getMapper(NoteSectionMapper.class);
 
 
-
+    List<NoteSectionResponseDto> mapNoteSectionListToNoteSectionResponseDtoList(List<NoteSection> noteSectionList);
 }
--- ThesisBackEnd/src/main/java/com/ComputerEngineeringDesign/Thesis/note/services/concretes/NoteSectionServiceImpl.java
+++ ThesisBackEnd/src/main/java/com/ComputerEngineeringDesign/Thesis/note/services/concretes/NoteSectionServiceImpl.java
@@ -1,14 +1,17 @@
 package com.ComputerEngineeringDesign.Thesis.note.services.concretes;
 
+import com.ComputerEngineeringDesign.Thesis.note.converters.NoteSectionMapper;
 import com.ComputerEngineeringDesign.Thesis.note.dtos.noteSectionDtos.NoteSectionResponseDto;
 import com.ComputerEngineeringDesign.Thesis.note.dtos.noteSectionDtos.NoteSectionSaveRequestDto;
 import com.ComputerEngineeringDesign.Thesis.note.dtos.noteSectionDtos.NoteSectionUpdateRequestDto;
+import com.ComputerEngineeringDesign.Thesis.note.entities.NoteSection;
 import com.ComputerEngineeringDesign.Thesis.note.repositories.NoteSectionDao;
 import com.ComputerEngineeringDesign.Thesis.note.services.abstracts.NoteSectionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -18,7 +21,9 @@ public class NoteSectionServiceImpl implements NoteSectionService {
 
     @Override
     public List<NoteSectionResponseDto> getAllNoteSections() {
-        return null;
+        List<NoteSection> noteSectionList =  noteSectionDao.findAll();
+        List<NoteSectionResponseDto> noteSectionResponseDtoList = NoteSectionMapper.INSTANCE.mapNoteSectionListToNoteSectionResponseDtoList(noteSectionList);
+        return noteSectionResponseDtoList;
     }
 
     @Override
